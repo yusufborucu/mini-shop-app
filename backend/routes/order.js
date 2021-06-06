0515c8877517b7b31e65716e8de38f028322c0ce
Yusuf Borucu <yusufborucu@gmail.com>
2021-06-06 12:15:40 +0300

    add order and get orders api

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -3,8 +3,23 @@ const router = express.Router();
 
 const Order = require('../models/Order');
 
-router.get('/', (req, res) => {
-  res.json({ message: 'success' });
+router.get('/', async (req, res) => {
+  const user_id = req.decode.userId;
+
+  const orders = await Order.find({ user_id });
+
+  res.json(orders);
+});
+
+router.post('/', async (req, res) => {
+  const user_id = req.decode.userId;
+  const data = req.body;
+  data.user_id = user_id;
+
+  const order = new Order(data);
+  await order.save();
+
+  res.json(order);
 });
 
 module.exports = router;
